91e953fa8094b10f96deca04e1eb463fa91a9084
Xojiakbar <x.nomonjonov@newuu.uz>
2025-08-29 14:44:00 +0500

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Xojiakbar No�Monjonov on 29/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
